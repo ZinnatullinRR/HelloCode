94af75eea8853a38d2084e46922c5124c0526f70
ZinnatullinRustem <sunnypenguin@mail.ru>
2022-09-01 05:08:42 +0500

    add Example005 Condition If-Else

---
+++ Example005_Condition_If_Else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() =="алиса")
+{
+    Console.WriteLine("Ура, это же АЛИСА!");
+}
+
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
